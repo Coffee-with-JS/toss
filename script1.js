bbfcb1ec3d8ea7c7d805314b1bee0e4bb84e05a6
Abhishek <theabhishek.srm@gmail.com>
2024-05-24 21:38:43 +0530

    toss m

--- script1.js
+++ script1.js
@@ -9,24 +9,30 @@ let scoreArea = document.querySelector('.scoreArea');
 // btns[1].addEventListener('click', toss)
 
 for(let t of btns){
-    t.addEventListener("click", toss)
+    t.addEventListener("click", (eventDetails)=>toss(eventDetails,10,20))
 }
 
 // function addEventListener(t1,t2){
+//   // t2  =  (eventDetails)=>toss(10,20)
 
+//     // t2(eventDetails)
+//       t2(eventDetails)
+   
+// }
 
-//     t2(eventDetails)
+// function t2(eventDetails){
+//      toss(eventDetails,10,20)
 // }
 
 
 
-function toss(eventDetails){
-    console.log(eventDetails);
+function toss(eventDetails, a,b){
+    console.log(eventDetails,a,b);
     // console.log(eventDetails.target)
     // let selectedElement = eventDetails.target;
     // let userChoice = selectedElement.innerText;
-    let userChoice = eventDetails.target.innerText
-    console.log(userChoice);
+    // let userChoice = eventDetails.target.innerText
+    // console.log(userChoice);
 }
 
 
